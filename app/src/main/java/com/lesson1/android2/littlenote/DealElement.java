8074ca74a84929a6fa0c6f06a96cbed49d43dc6c
gunahy <freshdv@mail.ru>
2016-09-14 13:38:57 +0500

    Полная синхронизация

--- app/src/main/java/com/lesson1/android2/littlenote/DealElement.java
+++ app/src/main/java/com/lesson1/android2/littlenote/DealElement.java
@@ -1,18 +1,22 @@
 package com.lesson1.android2.littlenote;
 
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class DealElement extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private TextView tvTitleNote, tvDescNote, tvDateNote, tvMap;
+    private ArrayAdapter elementAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deal_fragment);
+        setContentView(R.layout.activity_deal_element);
 
         initViews();
 
@@ -21,5 +25,10 @@ public class DealElement extends AppCompatActivity {
 
     private void initViews() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tvTitleNote = (TextView)findViewById(R.id.tvTitleNote);
+        tvDescNote = (TextView)findViewById(R.id.tvDescNote);
+        tvDateNote = (TextView)findViewById(R.id.tvDateNote);
+        tvMap = (TextView)findViewById(R.id.tvMap);
+
     }
 }
